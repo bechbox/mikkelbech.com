6c1149eadf247f043f0a8eebd9935fb2138197be
Mikkel Bech <mikkel@bechbox.dk>
2023-09-24 16:12:07 +0200

    Refactor sidebar and pages
    Refactored the sidebar component and the styles of the pages to improve readability and maintainability.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,158 +4,51 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+
+import EduIcon from "../../static/icons/regular/student.svg"
+import HouseIcon from "../../static/icons/regular/house-line.svg"
+import WrenchIcon from "../../static/icons/regular/wrench.svg"
+import CodeIcon from "../../static/icons/regular/code-simple.svg"
 
 const IndexPage = () => (
   <Layout>
     <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse risus
-      metus, dictum in tellus imperdiet, laoreet consectetur tortor. Maecenas
-      pellentesque vel enim vitae mollis. Duis maximus vitae nisl eget volutpat.
-      Nunc accumsan, lectus ac tempus tincidunt, nunc purus ultricies odio, nec
-      semper tortor ipsum at metus. Pellentesque habitant morbi tristique
-      senectus et netus et malesuada fames ac turpis egestas. Curabitur sed leo
-      neque. Duis id risus quam. Pellentesque viverra lacus consectetur mollis
-      fringilla. Praesent tortor lectus, consectetur non consectetur non, congue
-      et lacus. Nulla odio justo, iaculis faucibus massa nec, congue aliquam
-      massa. Aenean consectetur, odio auctor iaculis scelerisque, ex diam
-      efficitur nulla, non aliquam eros tortor dictum lacus. Cras euismod augue
-      fermentum sodales varius. Nullam quis odio molestie, lobortis leo ut,
-      sollicitudin quam. Vivamus et viverra metus, ac molestie ligula. Nullam
-      porta risus diam, eget aliquet odio interdum eu. Vestibulum lorem mi,
-      aliquet sed molestie vel, interdum sit amet purus. Sed in dictum felis. In
-      hac habitasse platea dictumst. Fusce nunc felis, posuere non sollicitudin
-      mollis, hendrerit pulvinar enim. Aliquam condimentum lectus mauris. Mauris
-      lacinia nulla sit amet massa euismod, sed tempor mi vestibulum. Aenean ac
-      nibh commodo, finibus justo at, vulputate nunc. Proin ac lectus vulputate,
-      aliquam nisl vitae, posuere odio. Etiam nunc ante, efficitur eget viverra
-      sed, cursus et neque. Proin aliquam odio a arcu scelerisque gravida. Nunc
-      tristique convallis quam, ut venenatis leo posuere sodales. Vivamus
-      fermentum quam lectus, ut interdum metus dictum sit amet. Duis quis ligula
-      vel nulla malesuada laoreet pharetra mattis dolor. Vestibulum elit neque,
-      suscipit in eros eget, malesuada varius eros. Sed sodales elit non aliquam
-      dapibus. Sed vestibulum vel massa non vestibulum. Donec sed neque a elit
-      elementum maximus ut vitae mauris. Maecenas vel sapien ut nunc pharetra
-      ornare. Nullam ut elit purus. Cras luctus neque tortor, quis finibus massa
-      aliquam at. In hac habitasse platea dictumst. Nunc sollicitudin, erat
-      scelerisque eleifend mollis, dui libero volutpat leo, et cursus diam arcu
-      vel est. Morbi tortor neque, auctor et tellus in, pretium vehicula arcu.
-      Nunc malesuada odio dapibus imperdiet molestie. Nunc et scelerisque
-      tortor, quis volutpat libero. Maecenas dapibus condimentum ipsum, ac
-      ornare odio. Nam imperdiet a felis ut malesuada. Donec ut euismod mi, eu
-      congue lectus. Donec eget diam consectetur nisi accumsan dignissim in quis
-      diam. Etiam dictum nibh dictum nulla volutpat rhoncus. Donec id tristique
-      magna. Nunc dapibus tempus nunc, ut pulvinar nunc sollicitudin vitae.
-      Praesent orci justo, pellentesque vitae enim et, fermentum rhoncus nisi.
-      Curabitur viverra dolor nec arcu dignissim, sed convallis elit dapibus.
-      Morbi eget est et tellus tristique egestas. Donec ut facilisis leo, vitae
-      ullamcorper dolor. Proin vulputate augue in mollis sodales. Cras orci mi,
-      mattis sed dolor gravida, hendrerit molestie libero. Nulla vehicula ante
-      mi, malesuada commodo nibh malesuada quis. Sed urna velit, fermentum sed
-      nulla et, pulvinar finibus enim. Donec et vestibulum odio. Fusce in arcu
-      maximus, facilisis turpis sit amet, porta diam. Vestibulum ante ipsum
-      primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam
-      tempus in sapien et consequat. Phasellus sit amet tortor massa. Ut magna
-      quam, pellentesque eu lobortis a, varius euismod purus. Cras tincidunt
-      nisi metus, ut iaculis urna fermentum eget. Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit. Suspendisse risus metus, dictum in tellus
-      imperdiet, laoreet consectetur tortor. Maecenas pellentesque vel enim
-      vitae mollis. Duis maximus vitae nisl eget volutpat. Nunc accumsan, lectus
-      ac tempus tincidunt, nunc purus ultricies odio, nec semper tortor ipsum at
-      metus. Pellentesque habitant morbi tristique senectus et netus et
-      malesuada fames ac turpis egestas. Curabitur sed leo neque. Duis id risus
-      quam. Pellentesque viverra lacus consectetur mollis fringilla. Praesent
-      tortor lectus, consectetur non consectetur non, congue et lacus. Nulla
-      odio justo, iaculis faucibus massa nec, congue aliquam massa. Aenean
-      consectetur, odio auctor iaculis scelerisque, ex diam efficitur nulla, non
-      aliquam eros tortor dictum lacus. Cras euismod augue fermentum sodales
-      varius. Nullam quis odio molestie, lobortis leo ut, sollicitudin quam.
-      Vivamus et viverra metus, ac molestie ligula. Nullam porta risus diam,
-      eget aliquet odio interdum eu. Vestibulum lorem mi, aliquet sed molestie
-      vel, interdum sit amet purus. Sed in dictum felis. In hac habitasse platea
-      dictumst. Fusce nunc felis, posuere non sollicitudin mollis, hendrerit
-      pulvinar enim. Aliquam condimentum lectus mauris. Mauris lacinia nulla sit
-      amet massa euismod, sed tempor mi vestibulum. Aenean ac nibh commodo,
-      finibus justo at, vulputate nunc. Proin ac lectus vulputate, aliquam nisl
-      vitae, posuere odio. Etiam nunc ante, efficitur eget viverra sed, cursus
-      et neque. Proin aliquam odio a arcu scelerisque gravida. Nunc tristique
-      convallis quam, ut venenatis leo posuere sodales. Vivamus fermentum quam
-      lectus, ut interdum metus dictum sit amet. Duis quis ligula vel nulla
-      malesuada laoreet pharetra mattis dolor. Vestibulum elit neque, suscipit
-      in eros eget, malesuada varius eros. Sed sodales elit non aliquam dapibus.
-      Sed vestibulum vel massa non vestibulum. Donec sed neque a elit elementum
-      maximus ut vitae mauris. Maecenas vel sapien ut nunc pharetra ornare.
-      Nullam ut elit purus. Cras luctus neque tortor, quis finibus massa aliquam
-      at. In hac habitasse platea dictumst. Nunc sollicitudin, erat scelerisque
-      eleifend mollis, dui libero volutpat leo, et cursus diam arcu vel est.
-      Morbi tortor neque, auctor et tellus in, pretium vehicula arcu. Nunc
-      malesuada odio dapibus imperdiet molestie. Nunc et scelerisque tortor,
-      quis volutpat libero. Maecenas dapibus condimentum ipsum, ac ornare odio.
-      Nam imperdiet a felis ut malesuada. Donec ut euismod mi, eu congue lectus.
-      Donec eget diam consectetur nisi accumsan dignissim in quis diam. Etiam
-      dictum nibh dictum nulla volutpat rhoncus. Donec id tristique magna. Nunc
-      dapibus tempus nunc, ut pulvinar nunc sollicitudin vitae. Praesent orci
-      justo, pellentesque vitae enim et, fermentum rhoncus nisi. Curabitur
-      viverra dolor nec arcu dignissim, sed convallis elit dapibus. Morbi eget
-      est et tellus tristique egestas. Donec ut facilisis leo, vitae ullamcorper
-      dolor. Proin vulputate augue in mollis sodales. Cras orci mi, mattis sed
-      dolor gravida, hendrerit molestie libero. Nulla vehicula ante mi,
-      malesuada commodo nibh malesuada quis. Sed urna velit, fermentum sed nulla
-      et, pulvinar finibus enim. Donec et vestibulum odio. Fusce in arcu
-      maximus, facilisis turpis sit amet, porta diam. Vestibulum ante ipsum
-      primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam
-      tempus in sapien et consequat. Phasellus sit amet tortor massa. Ut magna
-      quam, pellentesque eu lobortis a, varius euismod purus. Cras tincidunt
-      nisi metus, ut iaculis urna fermentum eget. Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit. Suspendisse risus metus, dictum in tellus
-      imperdiet, laoreet consectetur tortor. Maecenas pellentesque vel enim
-      vitae mollis. Duis maximus vitae nisl eget volutpat. Nunc accumsan, lectus
-      ac tempus tincidunt, nunc purus ultricies odio, nec semper tortor ipsum at
-      metus. Pellentesque habitant morbi tristique senectus et netus et
-      malesuada fames ac turpis egestas. Curabitur sed leo neque. Duis id risus
-      quam. Pellentesque viverra lacus consectetur mollis fringilla. Praesent
-      tortor lectus, consectetur non consectetur non, congue et lacus. Nulla
-      odio justo, iaculis faucibus massa nec, congue aliquam massa. Aenean
-      consectetur, odio auctor iaculis scelerisque, ex diam efficitur nulla, non
-      aliquam eros tortor dictum lacus. Cras euismod augue fermentum sodales
-      varius. Nullam quis odio molestie, lobortis leo ut, sollicitudin quam.
-      Vivamus et viverra metus, ac molestie ligula. Nullam porta risus diam,
-      eget aliquet odio interdum eu. Vestibulum lorem mi, aliquet sed molestie
-      vel, interdum sit amet purus. Sed in dictum felis. In hac habitasse platea
-      dictumst. Fusce nunc felis, posuere non sollicitudin mollis, hendrerit
-      pulvinar enim. Aliquam condimentum lectus mauris. Mauris lacinia nulla sit
-      amet massa euismod, sed tempor mi vestibulum. Aenean ac nibh commodo,
-      finibus justo at, vulputate nunc. Proin ac lectus vulputate, aliquam nisl
-      vitae, posuere odio. Etiam nunc ante, efficitur eget viverra sed, cursus
-      et neque. Proin aliquam odio a arcu scelerisque gravida. Nunc tristique
-      convallis quam, ut venenatis leo posuere sodales. Vivamus fermentum quam
-      lectus, ut interdum metus dictum sit amet. Duis quis ligula vel nulla
-      malesuada laoreet pharetra mattis dolor. Vestibulum elit neque, suscipit
-      in eros eget, malesuada varius eros. Sed sodales elit non aliquam dapibus.
-      Sed vestibulum vel massa non vestibulum. Donec sed neque a elit elementum
-      maximus ut vitae mauris. Maecenas vel sapien ut nunc pharetra ornare.
-      Nullam ut elit purus. Cras luctus neque tortor, quis finibus massa aliquam
-      at. In hac habitasse platea dictumst. Nunc sollicitudin, erat scelerisque
-      eleifend mollis, dui libero volutpat leo, et cursus diam arcu vel est.
-      Morbi tortor neque, auctor et tellus in, pretium vehicula arcu. Nunc
-      malesuada odio dapibus imperdiet molestie. Nunc et scelerisque tortor,
-      quis volutpat libero. Maecenas dapibus condimentum ipsum, ac ornare odio.
-      Nam imperdiet a felis ut malesuada. Donec ut euismod mi, eu congue lectus.
-      Donec eget diam consectetur nisi accumsan dignissim in quis diam. Etiam
-      dictum nibh dictum nulla volutpat rhoncus. Donec id tristique magna. Nunc
-      dapibus tempus nunc, ut pulvinar nunc sollicitudin vitae. Praesent orci
-      justo, pellentesque vitae enim et, fermentum rhoncus nisi. Curabitur
-      viverra dolor nec arcu dignissim, sed convallis elit dapibus. Morbi eget
-      est et tellus tristique egestas. Donec ut facilisis leo, vitae ullamcorper
-      dolor. Proin vulputate augue in mollis sodales. Cras orci mi, mattis sed
-      dolor gravida, hendrerit molestie libero. Nulla vehicula ante mi,
-      malesuada commodo nibh malesuada quis. Sed urna velit, fermentum sed nulla
-      et, pulvinar finibus enim. Donec et vestibulum odio. Fusce in arcu
-      maximus, facilisis turpis sit amet, porta diam. Vestibulum ante ipsum
-      primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam
-      tempus in sapien et consequat. Phasellus sit amet tortor massa. Ut magna
-      quam, pellentesque eu lobortis a, varius euismod purus. Cras tincidunt
-      nisi metus, ut iaculis urna fermentum eget.
+      <h1>Product. Design. Startup.</h1>
+      <div className="subTitle">
+        “The best way to predict the future is to invent it”, someone ...
+        perhaps Allan Kay, said once. I’m a firm believer the best way to learn
+        about the world is to build tools for people to use.
+      </div>
+      <div className="cv">
+        <div className="cv-item">
+          <div className="cv-icon">
+            <img src={EduIcon} alt="house icon" />
+          </div>
+          <div className="cv-title">
+            Master in Computer Science, University of Aarhus
+          </div>
+        </div>
+        <div className="cv-item">
+          <div className="cv-icon">
+            <img src={HouseIcon} alt="house icon" />
+          </div>
+          <div className="cv-title">Aarhus, Denmark</div>
+        </div>
+        <div className="cv-item">
+          <div className="cv-icon">
+            <img src={WrenchIcon} alt="house icon" />
+          </div>
+          <div className="cv-title">
+            Service Design, UI Design, JAMstack, Data Science, Photography
+          </div>
+        </div>
+        <div className="cv-item">
+          <div className="cv-icon">
+            <img src={CodeIcon} alt="house icon" />
+          </div>
+          <div className="cv-title">Javascript, CSS, Python</div>
+        </div>
+      </div>
     </div>
   </Layout>
 )
