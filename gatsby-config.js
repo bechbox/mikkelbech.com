759d14764fcff1bd81a03626566f1e116eeb4cca
Mikkel Bech <mikkel@bechbox.dk>
2023-09-28 22:23:56 +0200

    Update manifest and favicon
    Update the manifest file and favicon image.
    
    - Change the short name in the manifest file from 'starter' to 'home'.
    - Change the background color in the manifest file from '#663399' to '#121212'.
    - Update the favicon image.
    
    Resolves: #123

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,14 +30,14 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `home`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#121212`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: `#121212`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
   ],
