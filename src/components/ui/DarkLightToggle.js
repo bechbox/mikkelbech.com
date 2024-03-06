7475423602f386b89854afbf43c6aedd6d666eef
Mikkel Bech <mikkel@bechbox.dk>
2024-03-06 23:53:08 +0100

    Add Dark/Light mode toggle
    Added a new component `DarkLightToggle` that allows users to toggle between dark and light mode. The component uses the `useLocalStorage` hook to store the user's preference in local storage. The theme is applied to the `documentElement` using the `data-theme` attribute.

---
+++ src/components/ui/DarkLightToggle.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react"
+import { useLocalStorage } from "./useLocalStorage"
+
+import "../styles/darkToggleButton.scss"
+import { ReactComponent as SunIcon } from "../../../static/icons/regular/sun.svg"
+import { ReactComponent as MoonIcon } from "../../../static/icons/regular/moon.svg"
+
+export default function DarkLightToggle() {
+  const [checked, setChecked] = useLocalStorage("checked", false)
+
+  useEffect(() => {
+    if (checked) {
+      document.documentElement.setAttribute("data-theme", "dark")
+    } else {
+      document.documentElement.setAttribute("data-theme", "light")
+    }
+  }, [checked])
+
+  function handleChange(e) {
+    setChecked(e.target.checked)
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark")
+    } else {
+      document.documentElement.setAttribute("data-theme", "light")
+    }
+  }
+
+  return (
+    <div className="darkmode-toggle-button">
+      <input
+        type="checkbox"
+        id="darkmode-toggle"
+        checked={checked}
+        onChange={e => handleChange(e)}
+      />
+      <label htmlFor="darkmode-toggle">
+        <SunIcon className="sun" />
+        <MoonIcon className="moon" />
+      </label>
+    </div>
+  )
+}
